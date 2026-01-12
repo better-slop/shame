8897df4c5cf57369dee791590a2a4cc90e56d66a
cau1k <zero@composer.group>
2026-01-12 16:46:12 -0500

    feat: more styling

--- apps/web/src/components/icons/github.tsx
+++ apps/web/src/components/icons/github.tsx
@@ -1,18 +1,17 @@
-import React from "react";
 import type { SVGProps } from "react";
 
-export function DinkieIconsGithub(props: SVGProps<SVGSVGElement>) {
+export function GithubIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={12}
-      height={12}
-      viewBox="0 0 12 12"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
       {...props}
     >
       <path
         fill="currentColor"
-        d="M2 12h2v-1H3v-1H2V9H1V8h1v1h1v1h1V9h1V8H3V7H2V4h1V2h1v1h3V2h1v2h1v3H8v1H6v1h1v3h2v-1h1v-1h1V3h-1V2H9V1H2v1H1v1H0v7h1v1h1Zm0 0"
+        d="M23 9v6h-1v2h-1v2h-1v1h-1v1h-1v1h-2v1h-1v-5h-1v-1h1v-1h2v-1h1v-1h1V9h-1V6h-2v1h-1v1h-1V7h-4v1H9V7H8V6H6v3H5v5h1v1h1v1h2v2H7v-1H6v-1H4v1h1v2h1v1h3v3H8v-1H6v-1H5v-1H4v-1H3v-2H2v-2H1V9h1V7h1V5h1V4h1V3h2V2h2V1h6v1h2v1h2v1h1v1h1v2h1v2z"
       ></path>
     </svg>
   );
